a6234cee3a241ea1437d60a9b735bc14b51a9654
Fly Dev <dev@fly.io>
2025-06-22 16:09:25 +0000

    create categories migration

---
+++ priv/repo/migrations/20250622160919_create_categories.exs
@@ -0,0 +1,15 @@
+defmodule TodoApp.Repo.Migrations.CreateCategories do
+  use Ecto.Migration
+
+  def change do
+    create table(:categories) do
+      add :name, :string, null: false
+      add :slug, :string, null: false
+
+      timestamps()
+    end
+
+    create unique_index(:categories, [:name])
+    create unique_index(:categories, [:slug])
+  end
+end
