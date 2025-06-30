f6e7fd3dffb54d9a38bd520912c9bbb73626ff9a
Fly Dev <dev@fly.io>
2025-06-30 15:40:13 +0000

    fix page controller test

--- test/todo_app_web/controllers/page_controller_test.exs
+++ test/todo_app_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TodoAppWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Clinical Todo"
   end
 end
