199b4d4d21d2964b89a5921e44d7d763b2a88faa
Fly Dev <dev@fly.io>
2025-07-02 17:58:56 +0000

    add earmark dependency for markdown support

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule TodoApp.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"}
-    ]
+          {:earmark, "~> 1.4"},
+]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
