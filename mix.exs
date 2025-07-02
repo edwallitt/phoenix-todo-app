76ca47f2574b842339b0c938fdd028c9f8f1979d
Fly Dev <dev@fly.io>
2025-07-02 17:59:16 +0000

    fix syntax error add missing comma before earmark dep

--- mix.exs
+++ mix.exs
@@ -57,9 +57,9 @@ defmodule TodoApp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
-          {:earmark, "~> 1.4"},
-]
+      {:bandit, "~> 1.5"},
+      {:earmark, "~> 1.4"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
