84cc9cb6819dc1e1c69de2967a15a199be8ad8b4
Fly Dev <dev@fly.io>
2025-07-06 21:45:26 +0000

    remove sqlite3 dep, keep postgrex

--- mix.exs
+++ mix.exs
@@ -58,8 +58,7 @@ defmodule TodoApp.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:earmark, "~> 1.4"},
-      {:ecto_sqlite3, "~> 0.17"}
+      {:earmark, "~> 1.4"}
     ]
   end
 
